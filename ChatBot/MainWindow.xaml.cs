f31270469a65992a6705a0acae79f041f76716c2
Baxsh_cw07 <Baxsh_cw07@STHQ0127-05>
2023-03-03 15:35:35 +0400

    Add project files.

---
+++ ChatBot/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AIMLbot;
+
+namespace ChatBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Bot AI = new Bot();
+            AI.loadSettings();
+            AI.loadAIMLFromFiles();
+            AI.isAcceptingUserInput = false;
+            User myuser = new User("Username id", AI);
+            AI.isAcceptingUserInput = true;
+            Request r = new Request(textbox1.Text, myuser, AI);
+            Result res =AI.Chat(r);
+            textbox2.Text = "Tutorial bot : " + res.Output;
+        }
+    }
+}
